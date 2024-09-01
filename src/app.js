341d56a8db307a021614c06c64b52547f9876564
keshavsaini07 <sainikeshav770@gmail.com>
2024-09-01 21:41:50 +0530

    fix: changed code for deployment

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
+app.get('/', (req, res) => res.send("Welcome to the Crypto World"));
 app.use('/api', apiRoutes);
 
 (async () => {
